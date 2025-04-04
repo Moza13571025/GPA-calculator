2c6c8703c37115c40dbe1c71a8e99d3beddf5904
Moza <moza13571025@gmail.com>
2025-04-04 14:50:52 +0800

    功能：點擊新增鈕新增欄位，及動畫效果

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ allButtons.forEach((button) => {
 });
 
 //根據所選的成績變換背景顏色
-let allSelects = document.querySelectorAll("select"); //回傳NodeList
+let allSelects = document.querySelectorAll("select"); //回傳NodeList，靜態
 allSelects.forEach((e) => {
   e.addEventListener("change", (e) => {
     setGPA();
@@ -41,12 +41,12 @@ allSelects.forEach((e) => {
 });
 
 //當用戶改變credits輸入框的值，GPA更新
-let credits = document.querySelectorAll(".class-credit")
-credits.forEach((credit)=>{
-  credit.addEventListener("change",()=>{
+let credits = document.querySelectorAll(".class-credit");
+credits.forEach((credit) => {
+  credit.addEventListener("change", () => {
     setGPA();
-  })
-})
+  });
+});
 
 function changeColor(target) {
   if (target.value == "A" || target.value == "A-") {
@@ -134,3 +134,124 @@ function setGPA() {
 
   document.getElementById("result-gpa").innerText = result.toFixed(2);
 }
+
+let addButton = document.querySelector(".plus-btn");
+addButton.addEventListener("click", () => {
+  let newForm = document.createElement("form");
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("grader");
+
+  //新增五個元素
+  let newInput1 = document.createElement("input");
+  newInput1.setAttribute("type", "text");
+  newInput1.setAttribute("list", "opt");
+  newInput1.classList.add("class-type");
+
+  let newInput2 = document.createElement("input");
+  newInput2.setAttribute("type", "text");
+  newInput2.classList.add("class-number");
+
+  let newInput3 = document.createElement("input");
+  newInput3.setAttribute("type", "number");
+  newInput3.setAttribute("min", "0");
+  newInput3.setAttribute("max", "6");
+  newInput3.classList.add("class-credit");
+  newInput3.addEventListener("change", (e) => {
+    setGPA();
+  });
+
+  // select及其選項
+  let newSelect = document.createElement("select");
+  newSelect.classList.add("select");
+  var opt1 = document.createElement("option");
+  opt1.setAttribute("value", "");
+  let textNode1 = document.createTextNode("");
+  opt1.appendChild(textNode1);
+  var opt2 = document.createElement("option");
+  opt2.setAttribute("value", "A");
+  let textNode2 = document.createTextNode("A");
+  opt2.appendChild(textNode2);
+  var opt3 = document.createElement("option");
+  opt3.setAttribute("value", "A-");
+  let textNode3 = document.createTextNode("A-");
+  opt3.appendChild(textNode3);
+  var opt4 = document.createElement("option");
+  opt4.setAttribute("value", "B+");
+  let textNode4 = document.createTextNode("B+");
+  opt4.appendChild(textNode4);
+  var opt5 = document.createElement("option");
+  opt5.setAttribute("value", "B");
+  let textNode5 = document.createTextNode("B");
+  opt5.appendChild(textNode5);
+  var opt6 = document.createElement("option");
+  opt6.setAttribute("value", "B-");
+  let textNode6 = document.createTextNode("B-");
+  opt6.appendChild(textNode6);
+  var opt7 = document.createElement("option");
+  opt7.setAttribute("value", "C+");
+  let textNode7 = document.createTextNode("C+");
+  opt7.appendChild(textNode7);
+  var opt8 = document.createElement("option");
+  opt8.setAttribute("value", "C");
+  let textNode8 = document.createTextNode("C");
+  opt8.appendChild(textNode8);
+  var opt9 = document.createElement("option");
+  opt9.setAttribute("value", "C-");
+  let textNode9 = document.createTextNode("C-");
+  opt9.appendChild(textNode9);
+  var opt10 = document.createElement("option");
+  opt10.setAttribute("value", "D+");
+  let textNode10 = document.createTextNode("D+");
+  opt10.appendChild(textNode10);
+  var opt11 = document.createElement("option");
+  opt11.setAttribute("value", "D");
+  let textNode11 = document.createTextNode("D");
+  opt11.appendChild(textNode11);
+  var opt12 = document.createElement("option");
+  opt12.setAttribute("value", "D-");
+  let textNode12 = document.createTextNode("D-");
+  opt12.appendChild(textNode12);
+  var opt13 = document.createElement("option");
+  opt13.setAttribute("value", "F");
+  let textNode13 = document.createTextNode("F");
+  opt13.appendChild(textNode13);
+
+  newSelect.appendChild(opt1);
+  newSelect.appendChild(opt2);
+  newSelect.appendChild(opt3);
+  newSelect.appendChild(opt4);
+  newSelect.appendChild(opt5);
+  newSelect.appendChild(opt6);
+  newSelect.appendChild(opt7);
+  newSelect.appendChild(opt8);
+  newSelect.appendChild(opt9);
+  newSelect.appendChild(opt10);
+  newSelect.appendChild(opt11);
+  newSelect.appendChild(opt12);
+  newSelect.appendChild(opt13);
+
+  //allSelects的querySelectorAll是回傳靜態NodeList，所以newSelect要再設定事件監聽器
+  newSelect.addEventListener("change", (e) => {
+    //e不能省略
+    setGPA();
+    changeColor(e.target)
+  });
+
+  //垃圾桶按鈕
+  let newTrashButton = document.createElement("button");
+  newTrashButton.classList.add("trash-button");
+  let newITag = document.createElement("i");
+  newITag.classList.add("fas");
+  newITag.classList.add("fa-trash");
+  newTrashButton.appendChild(newITag);
+
+  newDiv.appendChild(newInput1);
+  newDiv.appendChild(newInput2);
+  newDiv.appendChild(newInput3);
+  newDiv.appendChild(newSelect);
+  newDiv.appendChild(newTrashButton);
+  newForm.appendChild(newDiv);
+  newForm.style.animation = "scaleUp 0.5s ease forwards"
+
+  document.querySelector(".all-inputs").appendChild(newForm);
+});
